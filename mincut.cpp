01165d53c4d9c06fbc5d077963cf6505df0a60b9
Chi <cychoi@zoho.com>
2016-09-30 05:45:30 -0600

    Add working and complete mincut.cpp

--- mincut.cpp
+++ mincut.cpp
@@ -32,6 +32,7 @@ int main() {
             adj[line].push_back(i);
         }
     }
+    cout << "Initial map size: " << adj.size() << endl;
     mapit it = adj.begin();
     cout << it->second.front() << endl;
     long cut = karger(adj);
@@ -39,7 +40,7 @@ int main() {
 //        cout << "Cut iteration: " << 1 << "\t" << "Min Cut: " << cut;
 //        cout << "\t" << "Node A: " << adj.begin()->first << endl;
 
-    for (unsigned long long i = 0; i != 100; ++i) {
+    for (unsigned long long i = 0; i != 1000; ++i) {
         cut = karger(adj);
         cout << "Cut iteration: " << i << "\t" << "Min Cut: " << cut;
         cout << "\t" << "Node A: " << adj.begin()->first << endl;
@@ -47,6 +48,7 @@ int main() {
             mincut = cut;
         }
     }
+    
     
     cout << "The minimum cut on this run is: " << mincut << endl;
 }
@@ -58,6 +60,7 @@ int karger(map <int, list<int> > a) {
         ++i;
     }
     cout << "Contractions: " << i << endl;
+    cout << "Final map size: " << a.size() << endl;
     return a.begin()->second.size();
 }
 
@@ -66,7 +69,9 @@ void contraction(map <int, list<int> >& a) {
 
     mapit random_node = a.begin();
     // generate random number out of size of a
-    cout << "Calling nrand(a.size())" << endl;
+
+ //   cout << "Calling nrand(a.size())" << endl;
+
     int random_node_select = nrand(a.size());
 //    cout << "random_node_select: " << random_node_select << endl;
     for (int i = 0; i != random_node_select; ++i) {
@@ -75,7 +80,9 @@ void contraction(map <int, list<int> >& a) {
 
     //generate random edge from the list associated with the random node
     listit random_edge = random_node->second.begin();
-    cout << "Calling nrand for edge" << endl;
+
+//    cout << "Calling nrand for edge" << endl;
+
     int random_edge_select = nrand(random_node->second.size());
 //    cout << "random_edge_select: " << random_edge_select << endl;
     for (int i = 0; i != random_edge_select; ++i) {
@@ -83,21 +90,29 @@ void contraction(map <int, list<int> >& a) {
     }
     int nodea = random_node->first;
     int nodeb = *random_edge;
-/*    
-    cout << "Random node: " << random_node->first << endl;
-    cout << "Random node: " << nodea << endl;
-    cout << "Random edge: " << *random_edge << endl;
-    cout << "Random edge: " << nodeb << endl;
-*/
+
+    
+//    cout << "Random node: " << random_node->first << endl;
+//    cout << "Random node: " << nodea << endl;
+//    cout << "Random edge: " << *random_edge << endl;
+//    cout << "Random edge: " << nodeb << endl;
+
 
     random_node->second.remove(nodeb); // removes nodea from nodeb connections
     a[nodeb].remove(nodea); // removes node a from node b
+
+    for (listit it = a[nodeb].begin(); it != a[nodeb].end(); ++it) {
+        a[*it].remove(nodeb);
+        a[*it].push_back(nodea);
+    }
     a[nodea].splice(a[nodea].begin(), a[nodeb]); // splices nodeb list onto nodea
     a.erase(nodeb);
+
+//    cout << "Map size following contraction: " << a.size() << endl;
 }
 
 int nrand(int n) {
-    cout << "Input n: " << n << endl;
+//    cout << "Input n: " << n << endl;
     if (n <= 0 || n > RAND_MAX) {
   //      throw domain_error("Argument to nrand is out of range");
         ++n;
